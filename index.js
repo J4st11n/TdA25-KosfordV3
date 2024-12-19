a97a5629dbef8a07ba7ed61e3fecac28dce0b87a
Nounejm <84151906+Nounejm@users.noreply.github.com>
2024-12-19 20:39:54 +0100

    V1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+let express = require('express');
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+    res.status(200).render('index');
+});
+
+app.get('/api', (req, res) => {
+    res.status(200).json({"organization": "Student Cyber Games"})
+});
+
+app.get('*', (req, res) => {
+    res.status(404).render('pagenotfound')
+});
+
+app.listen(3000)
