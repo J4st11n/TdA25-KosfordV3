4895cf3cb348d174e900dd4d46bf53d4f09026f9
Nounejm <84151906+Nounejm@users.noreply.github.com>
2025-01-06 17:24:55 +0100

    back

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ app.post('/api/v1/games', async (req, res) => {
         'message': 'Bad request: Missing field board'
     });
 
+
     if(!['beginner', 'easy', 'medium', 'hard', 'extreme'].includes(game_params.difficulty)) return res.status(422).json({
         'code': 422,
         'message': `Semantic error: Not a valid DifficultyType`
@@ -130,7 +131,7 @@ app.put('/api/v1/games/:game_id', async (req, res) => {
 
     let update = req.body;
 
-    if(!update?.name && !update?.difficulty && !update?.board && !update?.gameState) return res.status(400).json({
+    if(!update?.name && !update?.difficulty && !update?.board) return res.status(400).json({
         'code': 400,
         'message': 'Bad request: Missing GameCreateUpdateRequest'
     });
@@ -143,11 +144,6 @@ app.put('/api/v1/games/:game_id', async (req, res) => {
     if(update?.difficulty && !['beginner', 'easy', 'medium', 'hard', 'extreme'].includes(update.difficulty)) return res.status(422).json({
         'code': 422,
         'message': `Semantic error: Not a valid DifficultyType`
-    });
-
-    if(update?.gameState && !['opening', 'midgame', 'endgame', 'unknown'].includes(update.gameState)) return res.status(422).json({
-        'code': 422,
-        'message': `Semantic error: Not a valid gameState`
     });
 
     if(update?.board && (!Array.isArray(update.board) || update.board.length !== 15 || !update.board.every((row) => Array.isArray(row) && row.length === 15 && row.every(cell => cell === "X" || cell === "O" || cell === "")))) return res.status(422).json({
@@ -165,10 +161,6 @@ app.put('/api/v1/games/:game_id', async (req, res) => {
 
     if(update?.board){
         game.board = update.board;
-    };
-
-    if(update?.gameState){
-        game.gameState = update.gameState;
     };
 
     let current_date = new Date();
